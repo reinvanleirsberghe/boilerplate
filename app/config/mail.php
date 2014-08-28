931af87a37d111952c7b42e49c018be0beac5a22
reinvanleirsberghe <rein@Rein-2.local>
2014-08-28 21:08:31 +0200

    Config mail updates

--- app/config/mail.php
+++ app/config/mail.php
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'reinvanleirsberghe@gmail.com',
+	'username' => null,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'ziekdazeidegij',
+	'password' => null,
 
 	/*
 	|--------------------------------------------------------------------------
